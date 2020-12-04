edb0286f41a94c53bf359f78b6c0357a6ea3f395
Al-sany Samil <alsanysamil@gmail.com>
2020-12-04 19:04:51 +0600

    product list page adde

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router , Switch, Route }  from 'react-router-dom';
 import './App.css';
+import AdminSideMenu from './component/AdminSideMenu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">      
+        <Switch>
+          <route path="/admin/profile">
+            <AdminSideMenu component="AdminProfile"/>
+          </route>
+          <route path="/inventory/dashboard">
+            <AdminSideMenu component="InventoryDashboard"/>
+          </route>
+          <route path="/product/input">
+            <AdminSideMenu component="ProductInput"/>
+          </route>
+          <route path="/product/list">
+            <AdminSideMenu component="ProductList"/>
+          </route>
+          <route path="/farmer/register">
+            <AdminSideMenu component="FarmerRegistration"/>
+          </route>
+          <route path="/farmer/list">
+            <AdminSideMenu component="FarmerList"/>
+          </route>
+          <route path="/login">
+            <AdminSideMenu component="login"/>
+          </route>
+          <route path="/">
+            <AdminSideMenu />
+          </route>
+        </Switch>
+      </div>
+    </Router>          
   );
 }
 
